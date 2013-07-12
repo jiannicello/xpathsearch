ba001613646b33a56b646f49777a3590b5d360c2
John Iannicello <johnicello@gmail.com>
2013-07-11 23:51:04 -0400

    query_sources in search.js

--- site.js
+++ site.js
@@ -1,9 +1,14 @@
 ﻿'use strict';
 
 var connect = require('connect'),
+    query_sources = require('./lib/query_sources'),
     server = connect.createServer();
 
 server.use(connect.logger('dev'));
+
+server.use(connect.query());
+
+server.use(query_sources());
 
 server.use(connect.static(__dirname + '/site'));
 
--- site/js/search.js
+++ site/js/search.js
@@ -1,1 +1,57 @@
 ﻿var search = null;
+
+(function () {
+    function Search() {
+        console.log('Search');
+        
+        this.ui = {
+            btnDownload: $('#btnDownload'),
+            btnSearch: $('#btnSearch'),
+            ddl: $('#ddlSource'),
+            formDownload: $('#formDownload'),
+            results: $('#divResults'),
+            txtSearch: $('#txtSearch'),
+            txtSort: $('#txtSort')
+        };
+    }
+    
+    Search.prototype = {
+        ajaxGetDdl: function () {
+            console.log('Search ajaxGetDdl');
+            
+            $.ajax({
+                url: '?get_ddl_sources=1',
+                dataType: 'json',
+                success: $.proxy(this.onGetDdlAjax, this),
+                error: function () {
+                    console.error('error in ajax call');
+                }
+            });
+        },
+        onGetDdlAjax: function (o) {
+            console.log('Search onGetDdlAjax');
+            var ddl = this.ui.ddl,
+                btnSearch = this.ui.btnSearch;
+                
+            $.each(o, function () {
+                ddl.append('<option value="' + this.value + '">' + this.text + '</option');
+            });
+        },
+        render: function () {
+            console.group('Search render');
+            
+            //this.bindUI();
+            //this.setSaxonErrorHandler();
+            
+            this.ajaxGetDdl();
+            console.groupEnd();
+        }
+    };
+    
+    onSaxonLoad = function () {
+        console.log('onSaxonLoad');
+        search = new Search();
+        search.render();
+    }
+    
+} ());
