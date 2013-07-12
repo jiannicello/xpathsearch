f5c11ff5d599e7e8b2bc3be5bb6ff5d17392c544
John Iannicello <johnicello@gmail.com>
2013-07-11 21:01:55 -0400

    start of site.js and index page

---
+++ site.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+var connect = require('connect'),
+    server = connect.createServer();
+
+server.use(connect.logger('dev'));
+
+server.use(connect.static(__dirname + '/site'));
+
+server.listen(3000);
