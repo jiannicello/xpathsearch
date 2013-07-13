789f1410cb88628de87e9c7e4169328f1b3d2b63
John Iannicello <johnicello@gmail.com>
2013-07-13 01:04:15 -0400

    onBtnDownloadClick

--- site/js/search.js
+++ site/js/search.js
@@ -32,8 +32,65 @@
             console.log('Search bindUI');
             var ui = this.ui;
 
-            //ui.btnDownload.on('click', $.proxy(this.onBtnDownloadClick, this));
+            ui.btnDownload.on('click', $.proxy(this.onBtnDownloadClick, this));
             ui.btnSearch.on('click', $.proxy(this.onBtnSearchClick, this));
+        },
+        onBtnDownloadClick: function (e) {
+            console.log('Search onBtnDownloadClick');
+            var ui = this.ui,
+                hiddenData = ui.formDownload.find('#data'),
+                getJSONString = function () {
+                    var tbl = $('.table'),
+                        trL = tbl.find('tr'),
+                        data = {
+                            header: [],
+                            rows: []
+                        },
+                        getHeader = function (tr) {
+                            var thL = $(tr).find('th'),
+                                header = [];
+                            
+                            $.each(thL.slice(1), function () {
+                                header.push($(this).html());
+                            });
+                            
+                            return header;
+                        },
+                        getRow = function (tr) {
+                            var tdL = $(tr).find('td'),
+                                row = [];
+                            
+                            $.each(tdL.slice(1), function () {
+                                row.push($(this).html());
+                            });
+                            
+                            return row;
+                        };
+                    
+                    
+                    $.each(trL, function(index, tr) {
+                        var header = null;
+                        
+                        if (index === 0) {
+                            header = getHeader(tr);
+                            
+                            if (header.length > 0) {
+                                data.header = header;
+                            } else {
+                                data.rows.push(getRow(tr));
+                            }
+                        } else {
+                            data.rows.push(getRow(tr));
+                        }
+                    });
+                    
+                    return JSON.stringify(data);
+                }; //end getJSONString
+                
+            e.preventDefault();
+            
+            hiddenData.prop('value', getJSONString());
+            ui.formDownload.submit();
         },
         onBtnSearchClick: function (e) {
             console.log('Search onBtnSearchClick');
