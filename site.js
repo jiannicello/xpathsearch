133dfd8e21ff2355ef4d98e5460456cd3811da65
John Iannicello <johnicello@gmail.com>
2013-07-12 20:19:18 -0400

    query_xpath

--- site.js
+++ site.js
@@ -2,13 +2,14 @@
 
 var connect = require('connect'),
     query_sources = require('./lib/query_sources'),
+    query_xpath = require('./lib/query_xpath'),
     server = connect.createServer();
 
 server.use(connect.logger('dev'));
 
 server.use(connect.query());
-
 server.use(query_sources());
+server.use(query_xpath());
 
 server.use(connect.static(__dirname + '/site'));
 
