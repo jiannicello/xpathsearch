54b1fc03ab0c0734b2330ea585358781fae623bd
John Iannicello <johnicello@gmail.com>
2013-07-13 00:46:46 -0400

    post_download

--- site.js
+++ site.js
@@ -3,6 +3,7 @@
 var connect = require('connect'),
     query_sources = require('./lib/query_sources'),
     query_xpath = require('./lib/query_xpath'),
+    post_download = require('./lib/post_download'),
     server = connect.createServer();
 
 server.use(connect.logger('dev'));
@@ -10,6 +11,9 @@ server.use(connect.logger('dev'));
 server.use(connect.query());
 server.use(query_sources());
 server.use(query_xpath());
+
+server.use(connect.bodyParser());
+server.use(post_download());
 
 server.use(connect.static(__dirname + '/site'));
 
